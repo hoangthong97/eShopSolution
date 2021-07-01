0b5e9101084f6ee597706222a41ae44fce64ae04
Thông Hoàng <hoangthong97.th@gmail.com>
2021-07-01 10:07:42 +0700

    seed-data

---
+++ eShopSolution.Data/Migrations/20210701025057_Seed-Data-v1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class SeedDatav1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2021, 7, 1, 9, 50, 57, 239, DateTimeKind.Local).AddTicks(5752),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2021, 7, 1, 9, 32, 45, 576, DateTimeKind.Local).AddTicks(4059));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2021, 7, 1, 9, 32, 45, 576, DateTimeKind.Local).AddTicks(4059),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2021, 7, 1, 9, 50, 57, 239, DateTimeKind.Local).AddTicks(5752));
+        }
+    }
+}
---
+++ eShopSolution.Data/Migrations/20210701025342_Seed-Data-v2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class SeedDatav2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2021, 7, 1, 9, 53, 41, 616, DateTimeKind.Local).AddTicks(229),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2021, 7, 1, 9, 50, 57, 239, DateTimeKind.Local).AddTicks(5752));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2021, 7, 1, 9, 50, 57, 239, DateTimeKind.Local).AddTicks(5752),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2021, 7, 1, 9, 53, 41, 616, DateTimeKind.Local).AddTicks(229));
+        }
+    }
+}
